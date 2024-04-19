30e8a4544de6f08e85582f2929533a51bbf3ce5c
ChokoPieT <augoric81@gmail.com>
2024-04-19 17:40:45 +0300

    Add http file and correct mistakes

--- src/main/java/com/example/Spring/models/People.java
+++ src/main/java/com/example/Spring/models/People.java
@@ -1,19 +1,17 @@
 package com.example.Spring.models;
 
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Generated;
 
 @Data
 @Entity
+@Table(name="peoples")
 public class People {
     @Id
     @GeneratedValue
     private Long id;
     private String name;
     private String surname;
-    @Transient
     private int age;
     @Column(unique = true)
     private String email;
